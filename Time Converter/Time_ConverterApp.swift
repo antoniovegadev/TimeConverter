7b892e992ca2a4c0b32256d907be8e69e9cea74b
antonivegaochoa <tonivegaochoa@gmail.com>
2021-04-09 20:38:57 -0400

    Initial Commit

---
+++ Time Converter/Time_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_ConverterApp.swift
+//  Time Converter
+//
+//  Created by Antonio Vega on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Time_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
